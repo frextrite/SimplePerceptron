81123b18f6ca12405b36233c816d28eea44dbaa3
Amol <29180487+frextrite@users.noreply.github.com>
2017-12-18 21:59:56 +0530

    added member functions
    added train, predict, activate and linear_fx functions

--- perceptron.py
+++ perceptron.py
@@ -15,5 +15,33 @@ class Perceptron:
         learning_rate = 0.01
         print("Initial Weights: ", self.weights, " and bias ", self.bias)
 
+    def train(self, input_var, output):
+        v = 0
+        print("TRAINING")
+        for i in range(len(self.weights)):
+            v += (self.weights[i] * input_var[i])  # + bias[i]
+        predicted = self.activate(v)
+        error = output - predicted
+        change_in_weight = error * self.learning_rate
+        for i in range(len(self.weights)):
+            self.weights[i] = self.weights[i] + \
+                (change_in_weight * input_var[i])
+
+    def predict(self, test_var):
+        print("PREDICTING\n")
+        v = 0
+        for i in range(len(self.weights)):
+            v += (self.weights[i] * input_var[i])  # + bias[i]
+        output = self.activate(v)
+
+    def activate(self, n):
+        if(n > 0):
+            return 1
+        else:
+            return -1
+
+    def linear_fn(x):
+        y = 0.1*x + 0.5
+        return y
 
 p = Perceptron()
