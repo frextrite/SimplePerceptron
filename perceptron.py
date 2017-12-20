703263f102e00583ec82d8ff71588a2f1622314d
Amol <29180487+frextrite@users.noreply.github.com>
2017-12-20 11:47:19 +0530

    Added documentation
    Added documentation

--- perceptron.py
+++ perceptron.py
@@ -2,6 +2,7 @@ from random import *
 
 
 class Perceptron:
+    # Data members of perceptron class
     input_X_list = []
     input_answer_list = []
     test_list = []
@@ -9,19 +10,26 @@ class Perceptron:
     bias = []
     learning_rate = 0
 
+    # Constructor to initialise the data members
     def __init__(self):
         self.weights = [randint(-1, 1), randint(-1, 1)]
         self.bias = [randint(0, 1), randint(0, 1)]
-        learning_rate = 0.01
+        learning_rate = 0.01 # Industry standard?
         print("Initial Weights: ", self.weights, " and bias ", self.bias)
 
+        # Generate the train and testing list
         self.create_train_list()
         self.create_test_list()
+
+        # Train the model
         for input_list, output in zip(self.input_X_list, self.input_answer_list):
             self.train(input_list, output)
+
+        # Predicting values
         for test in self.test_list:
             self.predict(test)
 
+    # Generates a list of training data consisting of features and labels
     def create_train_list(self):
         for i in range(0, 10000):
             x = randint(-500000, 500000)
@@ -34,6 +42,7 @@ class Perceptron:
                 output = -1
             self.input_answer_list += [output]
 
+    # Generates a list of testing data
     def create_test_list(self):
         for i in range(0, 100):
             x = randint(-500000, 500000)
@@ -41,6 +50,7 @@ class Perceptron:
             inp = [x, y]
             self.test_list += [inp]
 
+    # includes feed forward and back propagation
     def train(self, input_var, output):
         v = 0
         # print("TRAINING")
@@ -53,6 +63,7 @@ class Perceptron:
             self.weights[i] = self.weights[i] + \
                 (change_in_weight * input_var[i])
 
+    # predicts the value of test data
     def predict(self, test_var):
         # print("PREDICTING\n")
         v = 0
@@ -60,16 +71,19 @@ class Perceptron:
             v += (self.weights[i] * test_var[i])  # + bias[i]
         output = self.activate(v)
 
+    # a simple activation function
     def activate(self, n):
         if(n > 0):
             return 1
         else:
             return -1
 
+    # a simple polynomial function for classifition purposes
     def polynomial(self, x):
         y = 2*x + 5
         return y
 
+# create the perceptron
 p = Perceptron()
 
 # TODO: create a graph for easy interpretation
